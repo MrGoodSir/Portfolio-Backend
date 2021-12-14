7b33c264d020d29e2be3a4dfe993be96e5543f1b
Daniel Hill <daniel.s.hill01@gmail.com>
2021-12-14 14:53:18 -0700

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 
 require("dotenv").config();
-const { PORT = 3001, DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -62,4 +62,4 @@ app.get("/projects", async (req, res) => {
 // LISTENER
 ////////////////////////////////
 
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+app.listen(process.env.PORT || 3001, () => console.log(`listening on PORT ${PORT}`));
